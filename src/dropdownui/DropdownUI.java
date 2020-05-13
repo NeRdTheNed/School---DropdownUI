1fd1dcb7e702482eb6705c88534146234dfd7898
Ned Loynd <41816363+NeRdTheNed@users.noreply.github.com>
2020-05-13 12:11:41 +1000

    Initial commit

---
+++ src/dropdownui/DropdownUI.java
@@ -0,0 +1,192 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dropdownui;
+
+//data storage imports
+import java.util.ArrayList;
+import java.util.HashMap;
+//javafx component imports
+import javafx.scene.Node;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Pagination;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.VBox;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+
+/**
+ *
+ * @author nedloynd
+ */
+
+public class DropdownUI {
+
+    /*
+    
+    The class DropdownUI handles:
+    the layout of components, 
+    construction of the accordian, 
+    and sending page requests / updating the pagination.
+    
+     */
+    
+    GridPane gpane;
+    Pagination pagenat;
+    public DropdownPageFactory processorFactory;
+    Accordion toAllLawsOfPhysics/*, a bee should not be able to fly.*/;
+
+    CommonGUIElements commonText = new CommonGUIElements();
+
+    private Accordion getAccordionOfActivities() {
+
+        /*
+        This accordion is only used internally right now, during the retrival of the gridPane.
+        As it simply contructs an accordian based on the registered activity groups,
+        it is better to interface with them, instead of the accordion based on them,
+        to ensure that everything is syncronised accross DropdownUI and DropdownPageFactory. 
+    
+        (I should probably add a more flexible way of contructing the accordian if I want to do other things with it though!)
+         */
+        
+        toAllLawsOfPhysics = new Accordion();
+
+        toAllLawsOfPhysics.setMinSize(100, 100);
+
+        HashMap<String, HashMap<String, ArrayList<Node>>> registeredAcivities = processorFactory.getActivityGroupList();
+
+        //not sure if bad code, or if just too abstracted
+        if (registeredAcivities.size() > 0) {
+
+            for (String key : registeredAcivities.keySet()) {
+
+                TitledPane planeToAdd = new TitledPane();
+                VBox listOfButtons = new VBox(10);
+                
+                planeToAdd.setText(key);
+
+                for (String activityName : registeredAcivities.get(key).keySet()) {
+
+                    Button toAdd = new Button("Open " + activityName + "...");
+
+                    toAdd.setOnAction(e -> updatePagesOnButtonPress(key, activityName));
+
+                    listOfButtons.getChildren().add(toAdd);
+                }
+
+                toAllLawsOfPhysics.getPanes().add(new TitledPane(key, listOfButtons));
+
+            }
+
+        } else {
+
+            //todo: add better error handling
+            toAllLawsOfPhysics.getPanes().add(new TitledPane("No activities found!", commonText.errorBox("activityList was empety!")));
+
+        }
+
+        return toAllLawsOfPhysics;
+
+    }
+
+    private void updatePagesOnButtonPress(String activityType, String activityName) {
+
+        //Sets the new current activity to the one passed by the button calling this method.
+        processorFactory.setCurrentActivity(activityType, activityName);
+        int foo = processorFactory.getAmountOfPagesInActivity(activityType, activityName);
+
+        /*
+        
+        Hacky code to ensure that the page always updates, as it doesn't update until a new page is retrived.
+        
+        Normally, you can update a pageination by changing the maximum page count.
+        However, if the page count doesn't change, then it doesn't update.
+        
+        For example, if you click a button, and it has 2 pages,
+        the pagination will set the maximum page count to 2.
+        Clicking another button that sets the maximum page count to 2 will also attempt to set the maximum page count,
+        but because it's the same (2), it doesn't update the maximum page count, so the pageination doesn't update.
+        As a result, no new page is retrivied. This means it will show the same page as before.
+        
+        This code ensures that the new maximum page count will always be different from the previous maximum page count,
+        by setting the maximum page count twice to two different values which will always differ by one.
+        This ensures that the maximum page count will always be set to two unique values,
+        and therefore allways differ from the maximum previous page count, as it can only have one value.
+        
+        This will, as said above, ensure that the page always updates, by always having a different maximum page count
+        to the previous maximum page count.
+        
+         */
+        
+        pagenat.setPageCount(foo + 1);
+        pagenat.setPageCount(foo);
+        pagenat.setCurrentPageIndex(0);
+
+        pagenat.setVisible(true);
+
+    }
+
+    public GridPane getActivityPane(String listTitle) {
+
+        /*
+        Constructs the layout of the GUI, and calls the creation of the accordian.
+        Should only be called after the items in a group are registered. 
+        
+        Acepts a String to be used as a "title" on top of the accordian.
+         */
+        
+        gpane = new GridPane();
+        
+        //redo this, it's a mess
+
+        ColumnConstraints leftCol = new ColumnConstraints(200);
+        ColumnConstraints rightCol = new ColumnConstraints(400);
+
+        leftCol.setPercentWidth(30);
+        rightCol.setPercentWidth(70);
+
+        RowConstraints topRow = new RowConstraints();
+        RowConstraints mainRowCons = new RowConstraints();
+
+        topRow.setPercentHeight(5);
+        mainRowCons.setPercentHeight(95);
+
+        gpane.getColumnConstraints().addAll(leftCol, rightCol);
+        gpane.getRowConstraints().addAll(topRow, mainRowCons);
+
+        gpane.setHgap(20);
+        gpane.setVgap(20);
+        gpane.setPadding(new Insets(20, 20, 20, 20));
+        gpane.setAlignment(Pos.CENTER);
+
+        //contructs the accordian when called
+        gpane.addRow(0, new Label(listTitle));
+        gpane.addRow(1, getAccordionOfActivities(), pagenat);
+
+        return gpane;
+
+    }
+
+    public DropdownUI() {
+
+        //fairly small initual setup, doesn't do much interesting apart from setting the pagination to invisible before the user clicks anything
+        
+        pagenat = new Pagination(0, 0);
+        processorFactory = new DropdownPageFactory();
+
+        pagenat.setPageFactory(processorFactory);
+        pagenat.setMinSize(100, 100);
+
+        pagenat.setVisible(false);
+
+    }
+
+}
---
+++ src/dropdownuiprojects/ChoreList.java
@@ -0,0 +1,149 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dropdownuiprojects;
+
+//javafx application imports
+import dropdownui.DropdownUI;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.Application;
+//DropdownPageUI related imports
+import java.util.ArrayList;
+import java.util.HashMap;
+import javafx.scene.Node;
+//all javafx geometry used in scene
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+/**
+ *
+ * @author nedloynd
+ */
+public class ChoreList extends Application {
+
+    //Used to register activities to DropdownUI, and display the resulting gridPane.
+    
+    @Override
+    public void start(Stage primaryStage) {
+
+        DropdownUI chorePageUI = new DropdownUI();
+
+        //label on top of accordian
+        String accordianTitle = "Isolation activities";
+        
+        ////start construction of all activity groups
+        
+        ///start construction of Homework activity group
+
+        /*create a hashmap containing all pages for the activity*/
+        HashMap<String, ArrayList<Node>> groupHomework = new HashMap<>();
+
+        //start construction of Maths activity
+        ArrayList<Node> activitiesToRegisterMaths = new ArrayList<>();
+        VBox mathTaskList = new VBox();
+
+        /*add the node to the hashmap of pages etc*/
+        activitiesToRegisterMaths.add(new Label("Here is all the Math homework that you need to do!"));
+        
+        mathTaskList.getChildren().add(new Label("1. Calculus"));
+        mathTaskList.getChildren().add(new Label("2. Trigonometry"));
+        mathTaskList.getChildren().add(new Label("3. Imaginary numbers"));
+        mathTaskList.getChildren().add(new Label("4. Etc"));
+
+        activitiesToRegisterMaths.add(mathTaskList);
+
+        /*add the activity (containing the pages constructed) to the group of activities*/
+        groupHomework.put("Maths", activitiesToRegisterMaths);
+
+        //end maths
+        //start english
+        ArrayList<Node> activitiesToRegisterEnglish = new ArrayList<>();
+        VBox englishTaskList = new VBox();
+
+        activitiesToRegisterEnglish.add(new Label("Here is all the English homework that you need to do!"));
+
+        englishTaskList.getChildren().add(new Label("1. Study Shakespaer"));
+        englishTaskList.getChildren().add(new Label("2. Write poetry"));
+        englishTaskList.getChildren().add(new Label("3. Etc"));
+
+        activitiesToRegisterEnglish.add(englishTaskList);
+
+        VBox shakespeareVBox = new VBox();
+        shakespeareVBox.setAlignment(Pos.CENTER);
+
+        /*hopefully Wikimedia doesn't change their url formats*/
+        String shakespeareUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Shakespeare.jpg/375px-Shakespeare.jpg";
+
+        Image shakespeareImg = new Image(shakespeareUrl, 400, 0, true, false);
+        ImageView imageView = new ImageView(shakespeareImg);
+        shakespeareVBox.getChildren().add(imageView);
+        shakespeareVBox.getChildren().add(new Label("A picture of Shakespeare!"));
+
+        activitiesToRegisterEnglish.add(shakespeareVBox);
+
+        groupHomework.put("English", activitiesToRegisterEnglish);
+
+        //end english
+        
+        /*register the group of activies in the processor factory*/
+        chorePageUI.processorFactory.registerActivityGroup("Homework", groupHomework);
+
+        ///end Homework
+        
+        ///start Music
+        HashMap<String, ArrayList<Node>> groupMusic = new HashMap<>();
+
+        //start practice
+        ArrayList<Node> activitiesToRegisterPractice = new ArrayList<>();
+        VBox musicPracticeTaskList = new VBox();
+
+        activitiesToRegisterPractice.add(new Label("Here is all the music practice that you need to do!"));
+
+        musicPracticeTaskList.getChildren().add(new Label("1. Practice scales"));
+        musicPracticeTaskList.getChildren().add(new Label("2. Practice piano pieces"));
+        musicPracticeTaskList.getChildren().add(new Label("3. Etc"));
+        
+        Label paddedPracticeEntry = new Label("Remember to have fun!");
+        paddedPracticeEntry.setPadding(new Insets(10, 0, 0, 0));
+        musicPracticeTaskList.getChildren().add(paddedPracticeEntry);
+
+        activitiesToRegisterPractice.add(musicPracticeTaskList);
+
+        groupMusic.put("music practice", activitiesToRegisterPractice);
+
+        //end practice
+        chorePageUI.processorFactory.registerActivityGroup("Music", groupMusic);
+
+        ///end Music,
+        ////end construction of all activity groups.
+        
+        //create the scene
+        
+        Scene scene = new Scene(chorePageUI.getActivityPane(accordianTitle), 800, 400);
+
+        primaryStage.setMinHeight(200);
+        primaryStage.setMinWidth(300);
+
+        primaryStage.setTitle("Activity chooser");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        launch(args);
+
+    }
+
+}
